5b19d4cbaad4bb7e9276d0c7306ae4d52cc869c1
Vlad <vlad.pbr@gmail.com>
2021-12-05 01:05:50 +0200

    feat(correct answer)

---
+++ 1/data/students/or/main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+void main() {
+
+    int firstNumber;
+    int secondNumber;
+
+    printf("first number: \n");
+    scanf("%d", &firstNumber);
+
+    printf("second number: \n");
+    scanf("%d", &secondNumber);
+
+    printf("the sum is: %d\n", firstNumber - secondNumber);
+
+}
